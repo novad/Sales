b49ddb510941d895c0df56b4b71dc571ec3155e8
Daniel <danielhnova@gmail.com>
2019-06-23 21:46:15 +0200

    Update RoundingTest.java
    Method name refactored

--- src/test/java/utils/RoundingTest.java
+++ src/test/java/utils/RoundingTest.java
@@ -9,7 +9,7 @@ public class RoundingTest {
     public void testRounding(){
         // Test rounding to *.*5 (rounding up to 0.05 if x>=0.25)
         float input = 1.925f;
-        float output = Utils.roundTwoDecimals(input);
+        float output = Utils.roundClosestDecimal(input);
         Assert.assertEquals(output, 1.95f, 0);
     }
 
@@ -17,7 +17,7 @@ public class RoundingTest {
     public void testRoundingToLower(){
         // Test rounding for lower value (flooring)
         float input = 1.924f;
-        float output = Utils.roundTwoDecimals(input);
+        float output = Utils.roundClosestDecimal(input);
         Assert.assertEquals(output, 1.90f, 0);
     }
 
@@ -25,7 +25,7 @@ public class RoundingTest {
     public void testRoundingToNoDecimals(){
         // Test rounding resulting in no decimals
         float input = 3.999f;
-        float output = Utils.roundTwoDecimals(input);
+        float output = Utils.roundClosestDecimal(input);
         Assert.assertEquals(output, 4f, 0);
     }
 
