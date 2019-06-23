982c816da63b89ce09b3e0e547b745cb97c7b29f
Daniel <danielhnova@gmail.com>
2019-06-23 21:48:12 +0200

    Updated test SalesPointTest.java
    Created givenTest3 to test the third given example. TODO: there's a discrepancy with the description, discuss.
    
    Reorganized products to test.

--- src/test/java/SalesPointTest.java
+++ src/test/java/SalesPointTest.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import taxing.ItalyTaxing;
+import utils.Utils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -15,13 +16,15 @@ public class SalesPointTest {
     Book bookSolitude = new Book(1, "One Thousand Years of Solitude", 20, Country.ITALY);
     Book bookQuixote = new Book(2, "Don Quixote", 17, Country.SPAIN);
     Food chocolateImported = new Food(3, "Imported Chocolate box", 10, Country.GERMANY);
-    Food chocolateBarLocal = new Food(4, "Chocolate bar", 0.85f, Country.ITALY);
-    Food foodIta2 = new Food(5, "Pecorino", 8, Country.ITALY);
-    Medicine medicine_imported = new Medicine(6, "Sunscreen 50", 18f, Country.FRANCE);
-    Medicine medicine = new Medicine(7, "Headache Pills", 9.75f, Country.ITALY);
-    Product musicCD = new Product(9, "Music CD", 14.99f, Country.ITALY);
-    Product perfumeImported = new Product(10, "Imported bottle of perfume", 47.5f, Country.FRANCE);
-    Product perfumeLocal = new Product(11, "Bottle of perfume", 18.99f, Country.ITALY);
+    Food chocolateImportedExpensive = new Food(4, "Imported Chocolate box 2", 11.25f, Country.US);
+    Food chocolateBarLocal = new Food(5, "Chocolate bar", 0.85f, Country.ITALY);
+    Food foodIta2 = new Food(6, "Pecorino", 8, Country.ITALY);
+    Medicine medicine_imported = new Medicine(7, "Sunscreen 50", 18f, Country.FRANCE);
+    Medicine medicine = new Medicine(8, "Headache Pills", 9.75f, Country.ITALY);
+    Product musicCD = new Product(10, "Music CD", 14.99f, Country.ITALY);
+    Product perfumeImported = new Product(11, "Imported bottle of perfume", 27.99f, Country.FRANCE);
+    Product perfumeImportedExpensive = new Product(12, "Imported bottle of expensive perfume", 47.5f, Country.FRANCE);
+    Product perfumeLocal = new Product(13, "Bottle of perfume", 18.99f, Country.ITALY);
 
 
     private SalesPoint salesPoint;
@@ -55,7 +58,7 @@ public class SalesPointTest {
 
         Sale newSale = new Sale(sdf.format(new Date()));
         newSale.addPurchase(chocolateImported, 1);
-        newSale.addPurchase(perfumeImported, 1);
+        newSale.addPurchase(perfumeImportedExpensive, 1);
 
         Sale sale = this.salesPoint.finalizeSale(newSale);
 
@@ -64,5 +67,34 @@ public class SalesPointTest {
         Assert.assertEquals(sale.getTotalTaxes(), 7.65f, 0);
         Assert.assertEquals(sale.getTotal(), 65.15f, 0);
 
+    }
+
+    /* TODO: discuss.
+     /  There's a discrepancy in this test w.r.t. the given tests:
+        > 3 boxes of imported chocolate have an unitary value of 11.25
+        > no base sale tax is paid because it's food
+        > an import tax of 0.05 has to be paid, so:
+            (11.25*3) * 1.05 = 35.4375
+          Rounded to closest 0.05:
+            35.45
+          The given examples stated 35.55
+     */
+    @Test
+    public void givenTest3(){
+
+        Sale newSale = new Sale(sdf.format(new Date()));
+        newSale.addPurchase(perfumeImported, 1);
+        newSale.addPurchase(perfumeLocal, 1);
+        newSale.addPurchase(medicine, 1);
+        newSale.addPurchase(chocolateImportedExpensive, 3);
+
+        Sale sale = this.salesPoint.finalizeSale(newSale);
+
+        this.salesPoint.printReceipt(newSale, new ConcreteRecepitGenerator());
+
+
+        // Check totals
+        Assert.assertEquals(sale.getTotalTaxes(), 7.8f, 0);
+        Assert.assertEquals(sale.getTotal(), 98.28f, 0);
     }
 }
