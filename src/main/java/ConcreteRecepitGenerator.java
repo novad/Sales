3fc84c6f9bdb148e60bcf5ae199df088f77c97c0
Daniel <danielhnova@gmail.com>
2019-06-23 20:57:37 +0200

    Create ConcreteRecepitGenerator.java
    Created a concrete receipt printing class

---
+++ src/main/java/ConcreteRecepitGenerator.java
@@ -0,0 +1,23 @@
+import model.Sale;
+import model.SaleItem;
+
+public class ConcreteRecepitGenerator implements ReceiptGenerator {
+    @Override
+    public void printReceipt(Sale sale) {
+
+        System.out.println("===================================");
+        System.out.println("Receipt of sale - " + sale.getSaleDate());
+        System.out.println("===================================");
+        for(SaleItem item : sale.getShoppingCart()){
+            float totalPrizeItem = item.getSubTotal()+item.getTaxesTotal();
+            System.out.println(" - " +
+                    item.getUnits() + "x" +
+                    item.getProduct().getName() +
+                    ": " + totalPrizeItem );
+        }
+        System.out.println();
+        System.out.println("Total Taxes: " + sale.getSubTotal());
+        System.out.println("Total: " + sale.getTotal());
+        System.out.println("===================================");
+    }
+}
