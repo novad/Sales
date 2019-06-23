7e58a7544d772a3a81057c3e2f0d73061b7e88d9
Daniel <danielhnova@gmail.com>
2019-06-23 18:22:58 +0200

    Added SaleItem Class

---
+++ src/main/java/model/SaleItem.java
@@ -0,0 +1,36 @@
+package model;
+
+import model.Product;
+
+public class SaleItem {
+    private Product product;
+    private int units;
+    private float subTotal;
+    private float taxesTotal;
+
+    SaleItem(Product product, int units) {
+        this.product = product;
+        this.units = units;
+        this.subTotal = product.getShelfPrice() * this.units;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public float getSubTotal(){
+        return this.subTotal;
+    }
+
+    public float getTaxesTotal() {
+        return taxesTotal;
+    }
+
+    public void setTaxesTotal(float taxesTotal) {
+        this.taxesTotal = taxesTotal;
+    }
+}
