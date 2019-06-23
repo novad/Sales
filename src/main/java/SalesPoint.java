b3e4ab89e4fdb2429e4ae0df750fb65a2072bfb1
Daniel <danielhnova@gmail.com>
2019-06-23 18:31:49 +0200

    Created SalesPoint.java
    Created the studs SalesPoint class that will work as a register, it will total the amount to pay and calls the interface to prints receipts.

--- src/main/java/SalesPoint.java
+++ src/main/java/SalesPoint.java
@@ -1,13 +1,30 @@
-import java.io.PrintStream;
+import model.Sale;
+import taxing.TaxingItemPolicy;
 
+/**
+ * Represents the point of sale of the store
+ */
 public class SalesPoint {
-    public static void main(String[] args)
-    {
-        print(System.out);
+
+    /**
+     * Taxing policy interface for the store
+     */
+    private TaxingItemPolicy taxingScheme;
+
+    public SalesPoint(TaxingItemPolicy taxingScheme) {
+        this.taxingScheme = taxingScheme;
     }
 
-    public static void print(PrintStream out){
-        out.print("test");
 
+    /**
+     * End a sale and calculate subotal, taxes, and total.
+     * @param sale Sale to finalize
+     * @return Finalized sale
+     */
+    public Sale finalizeSale(Sale sale){
+        // TODO: implement
+        return null;
     }
+
+
 }
